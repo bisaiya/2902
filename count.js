b798f277324b443815ead1e7c20d99f93c587640
bisaiya <bisaiya87@gmail.com>
2020-10-30 20:45:52 +0700

    countdown

--- count.js
+++ count.js
@@ -1,15 +1,14 @@
 window.onload = function() {
-	countUpFromTime('Feb 29, 2020 23:57:00', 'countup1');
+	countDownFromTime('Mar 8, 2021 00:00:00', 'countdownId');
 };
 
-function countUpFromTime(countFrom, id) {
-	countFrom = new Date(countFrom).getTime();
+function countDownFromTime(countDownDate, id) {
+	countDownDate = new Date(countDownDate).getTime();
 
-	var now = new Date(),
-		countFrom = new Date(countFrom),
-		timeDifference = now - countFrom;
+	let now = new Date(),
+		timeDifference = countDownDate - now;
 
-	var secondsInADay = 60 * 60 * 1000 * 24,
+	let secondsInADay = 60 * 60 * 1000 * 24,
 		secondsInAHour = 60 * 60 * 1000;
 
 	days = Math.floor(timeDifference / secondsInADay * 1);
@@ -17,14 +16,14 @@ function countUpFromTime(countFrom, id) {
 	mins = Math.floor(((timeDifference % secondsInADay) % secondsInAHour) / (60 * 1000) * 1);
 	secs = Math.floor((((timeDifference % secondsInADay) % secondsInAHour) % (60 * 1000)) / 1000 * 1);
 
-	let idEl = document.querySelector('#countup1');
+	let idEl = document.querySelector('#countdownId');
 	idEl.querySelectorAll('.days')[0].innerHTML = days;
 	idEl.querySelectorAll('.hours')[0].innerHTML = hours;
 	idEl.querySelectorAll('.minutes')[0].innerHTML = mins;
 	idEl.querySelectorAll('.seconds')[0].innerHTML = secs;
 
-	clearTimeout(countUpFromTime.intervel);
-	countUpFromTime.intervel = setTimeout(function() {
-		countUpFromTime(countFrom, id);
+	clearTimeout(countDownFromTime.intervel);
+	countDownFromTime.intervel = setTimeout(function() {
+		countDownFromTime(countDownDate, id);
 	}, 1000);
 }
